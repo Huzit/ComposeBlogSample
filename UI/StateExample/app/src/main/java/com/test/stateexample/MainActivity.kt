6440a7bdd54523dca9f70ba59a1e770edf089f07
박상환 <hwan4789@gmail.com>
2024-03-20 17:57:32 +0900

    flow 예제 작성 중

--- UI/StateExample/app/src/main/java/com/test/stateexample/MainActivity.kt
+++ UI/StateExample/app/src/main/java/com/test/stateexample/MainActivity.kt
@@ -12,14 +12,22 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.test.stateexample.ui.theme.StateExampleTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +39,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting2 ()
+                    LiveDataSample ()
                 }
             }
         }
@@ -43,11 +51,11 @@ fun Greeting() {
     var name by remember { mutableStateOf("") }
     Column(modifier = Modifier.padding(16.dp)) {
         if(name.isNotEmpty())
-        Text(
-            text = "Hello! $name",
-            modifier = Modifier.padding(bottom = 8.dp),
-            style = MaterialTheme.typography.bodyMedium
-        )
+            Text(
+                text = "Hello! $name",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.bodyMedium
+            )
         OutlinedTextField(
             value = name,
             onValueChange = { name = it},
@@ -69,6 +77,36 @@ fun Greeting2() {
         OutlinedTextField(
             value = name,
             onValueChange = { name = it},
+            label = { Text("Name")}
+        )
+    }
+}
+
+//플로우 예제
+@Composable
+fun FlowSample(){
+    val a: Flow<String> = flow {
+        delay(1000L)
+        emit("이게 플로우여")
+    }
+    a.collectAsState(initial = "")
+}
+
+//라이브데이터 예제
+val name = MutableLiveData<String>()
+@Composable
+fun LiveDataSample(){
+    val name1 by name.observeAsState()
+    Column (modifier = Modifier.padding(16.dp)) {
+        if(name1 != null)
+            Text(
+                text = "Hello! $name1",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.bodyMedium
+            )
+        OutlinedTextField(
+            value = name1 ?: "",
+            onValueChange = { name.postValue(it)},
             label = { Text("Name")}
         )
     }
