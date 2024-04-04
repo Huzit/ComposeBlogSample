001cc82104ff40f24d47e1003ccc9d5be221aea7
박상환 <hwan4789@gmail.com>
2024-04-04 17:20:46 +0900

    rememberSaveable 작성 완료

--- Concept/State/StateExample/app/src/main/java/com/test/stateexample/MainActivity.kt
+++ Concept/State/StateExample/app/src/main/java/com/test/stateexample/MainActivity.kt
@@ -1,40 +1,39 @@
 package com.test.stateexample
 
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.mapSaver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.test.stateexample.ui.theme.StateExampleTheme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.FlowCollector
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.parcelize.Parcelize
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,16 +45,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    FlowSample ()
+                    ListSaverSnippet()
                 }
             }
         }
     }
 }
+
+@Composable
+fun HoistingSnippets(){
+    var name by rememberSaveable { mutableStateOf("") }
+
+    ComposableSnippet(name = name) {
+        name = it
+    }
+}
+
 //작동 안되는거
 @Composable
-fun Greeting() {
-    var name by remember { mutableStateOf("") }
+fun ComposableSnippet(name: String, onValueChanged: (String) -> Unit) {
     Column(modifier = Modifier.padding(16.dp)) {
         if(name.isNotEmpty())
             Text(
@@ -65,14 +73,14 @@ fun Greeting() {
             )
         OutlinedTextField(
             value = name,
-            onValueChange = { name = it},
+            onValueChange = onValueChanged,
             label = { Text("Name")}
         )
     }
 }
 //작동 되는거
 @Composable
-fun Greeting2() {
+fun ComposableSnippet2() {
     var name by remember { mutableStateOf("") }
     Column(modifier = Modifier.padding(16.dp)) {
         if(name.isNotEmpty())
@@ -87,6 +95,34 @@ fun Greeting2() {
             label = { Text("Name")}
         )
     }
+}
+
+//플로우 예제
+@Composable
+fun FlowSnippet(){
+    var name1 by remember { mutableStateOf("플로우 수집 아직 안됬다잉") }
+    val a: Flow<String> = flow {
+        while(true){
+            emit("이게 플로우여")
+            delay(5000L)
+        }
+    }
+    //그냥 flow를 collect 했을 경우
+    LaunchedEffect(key1 = name1) {
+        a.collect{
+            name1 = it
+        }
+    }
+    Text(
+        text = "Hello! ${name1}",
+        modifier = Modifier.padding(bottom = 8.dp),
+        style = MaterialTheme.typography.bodyMedium
+    )
+    runBlocking {
+        delay(3000)
+    }
+    //state로 collect 했을 경우
+    name1 = a.collectAsState(initial = "아직 안된 거시여").value
 }
 
 //플로우 예제
@@ -106,14 +142,14 @@ fun FlowSample(){
             sampleText = it
         }
     }*/
-    
+
     Text(text = sampleText)
 }
 
 //라이브데이터 예제
 val name = MutableLiveData<String>()
 @Composable
-fun LiveDataSample(){
+fun LiveDataSnippet(){
     val name1 by name.observeAsState()
     Column (modifier = Modifier.padding(16.dp)) {
         if(name1 != null)
@@ -130,10 +166,65 @@ fun LiveDataSample(){
     }
 }
 
+@Composable
+fun SavableSnippet(){
+    var name by remember{ mutableStateOf("1") }
+    var nameWithSavable by rememberSaveable{ mutableStateOf("1") }
+    Column {
+        Text(text = name)
+        Button(onClick = { name += "1" }) {
+            Text(text = "add_button")
+        }
+    }
+}
+
+@Parcelize
+data class City(val name: String, val country: String): Parcelable
+
+@Composable
+fun ParcelizeSnippet(){
+    var selectedCity = rememberSaveable {
+        mutableStateOf(City("울산", "울산광역시"))
+    }
+    Text(text = selectedCity.value.country)
+}
+
+data class Region(val name: String, val country: String)
+
+val RegionSaver = run{
+    val nameKey = "Name"
+    val countryKey = "Country"
+    mapSaver(
+        save = {mapOf(nameKey to it.name, countryKey to it.country)},
+        restore = {Region(it[nameKey] as String, it[countryKey] as String)}
+    )
+}
+
+@Composable
+fun MapSaverSnippet(){
+    val selectedCity = rememberSaveable(stateSaver = RegionSaver) {
+        mutableStateOf(Region("Madrid", "울산"))
+    }
+    Text(text = selectedCity.value.toString())
+}
+
+val RegionSaver2 = listSaver<Region, Any>(
+    save = { listOf(it.name, it.country) },
+    restore = {Region(it[0] as String, it[1] as String)} 
+)
+@Composable
+fun ListSaverSnippet(){
+    val selectedCity = rememberSaveable(stateSaver = RegionSaver2) {
+        mutableStateOf(Region("울산", "광역시"))
+    }
+    
+    Text(text = selectedCity.value.toString())
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     StateExampleTheme {
-        Greeting()
     }
 }
--- UI/StateExample/app/src/main/java/com/test/stateexample/MainActivity.kt
+++ UI/StateExample/app/src/main/java/com/test/stateexample/MainActivity.kt
@@ -1,33 +1,38 @@
 package com.test.stateexample
 
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.mapSaver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.test.stateexample.ui.theme.StateExampleTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import kotlinx.parcelize.Parcelize
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,16 +44,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LiveDataSample ()
+                    ListSaverSnippet()
                 }
             }
         }
     }
 }
+
+@Composable
+fun HoistingSnippets(){
+    var name by rememberSaveable { mutableStateOf("") }
+
+    ComposableSnippet(name = name) {
+        name = it
+    }
+}
+
 //작동 안되는거
 @Composable
-fun Greeting() {
-    var name by remember { mutableStateOf("") }
+fun ComposableSnippet(name: String, onValueChanged: (String) -> Unit) {
     Column(modifier = Modifier.padding(16.dp)) {
         if(name.isNotEmpty())
             Text(
@@ -58,14 +72,14 @@ fun Greeting() {
             )
         OutlinedTextField(
             value = name,
-            onValueChange = { name = it},
+            onValueChange = onValueChanged,
             label = { Text("Name")}
         )
     }
 }
 //작동 되는거
 @Composable
-fun Greeting2() {
+fun ComposableSnippet2() {
     var name by remember { mutableStateOf("") }
     Column(modifier = Modifier.padding(16.dp)) {
         if(name.isNotEmpty())
@@ -84,18 +98,36 @@ fun Greeting2() {
 
 //플로우 예제
 @Composable
-fun FlowSample(){
+fun FlowSnippet(){
+    var name1 by remember { mutableStateOf("플로우 수집 아직 안됬다잉") }
     val a: Flow<String> = flow {
-        delay(1000L)
-        emit("이게 플로우여")
+        while(true){
+            emit("이게 플로우여")
+            delay(5000L)
+        }
     }
-    a.collectAsState(initial = "")
+    //그냥 flow를 collect 했을 경우
+    LaunchedEffect(key1 = name1) {
+        a.collect{
+            name1 = it
+        }
+    }
+    Text(
+        text = "Hello! ${name1}",
+        modifier = Modifier.padding(bottom = 8.dp),
+        style = MaterialTheme.typography.bodyMedium
+    )
+    runBlocking {
+        delay(3000)
+    }
+    //state로 collect 했을 경우
+    name1 = a.collectAsState(initial = "아직 안된 거시여").value
 }
 
 //라이브데이터 예제
 val name = MutableLiveData<String>()
 @Composable
-fun LiveDataSample(){
+fun LiveDataSnippet(){
     val name1 by name.observeAsState()
     Column (modifier = Modifier.padding(16.dp)) {
         if(name1 != null)
@@ -112,10 +144,65 @@ fun LiveDataSample(){
     }
 }
 
+@Composable
+fun SavableSnippet(){
+    var name by remember{ mutableStateOf("1") }
+    var nameWithSavable by rememberSaveable{ mutableStateOf("1") }
+    Column {
+        Text(text = name)
+        Button(onClick = { name += "1" }) {
+            Text(text = "add_button")
+        }
+    }
+}
+
+@Parcelize
+data class City(val name: String, val country: String): Parcelable
+
+@Composable
+fun ParcelizeSnippet(){
+    var selectedCity = rememberSaveable {
+        mutableStateOf(City("울산", "울산광역시"))
+    }
+    Text(text = selectedCity.value.country)
+}
+
+data class Region(val name: String, val country: String)
+
+val RegionSaver = run{
+    val nameKey = "Name"
+    val countryKey = "Country"
+    mapSaver(
+        save = {mapOf(nameKey to it.name, countryKey to it.country)},
+        restore = {Region(it[nameKey] as String, it[countryKey] as String)}
+    )
+}
+
+@Composable
+fun MapSaverSnippet(){
+    val selectedCity = rememberSaveable(stateSaver = RegionSaver) {
+        mutableStateOf(Region("Madrid", "울산"))
+    }
+    Text(text = selectedCity.value.toString())
+}
+
+val RegionSaver2 = listSaver<Region, Any>(
+    save = { listOf(it.name, it.country) },
+    restore = {Region(it[0] as String, it[1] as String)} 
+)
+@Composable
+fun ListSaverSnippet(){
+    val selectedCity = rememberSaveable(stateSaver = RegionSaver2) {
+        mutableStateOf(Region("울산", "광역시"))
+    }
+    
+    Text(text = selectedCity.value.toString())
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     StateExampleTheme {
-        Greeting()
     }
 }
